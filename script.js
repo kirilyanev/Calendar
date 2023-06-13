1b1bc30016a9074a5e4eb6f1a5f1c91fdc8e8fc8
Kiril Yanev <kl_yanev@abv.bg>
2023-06-13 22:16:40 +0300

    Create script.js and style.css

---
+++ script.js
@@ -0,0 +1,52 @@
+function createCalendar(year, month) {
+    const calendar = document.getElementById("calendar");
+  
+    // Get the number of days in the month
+    const numDays = new Date(year, month + 1, 0).getDate();
+  
+    // Get the first day of the month (0 - Sunday, 1 - Monday, etc.)
+    const firstDay = new Date(year, month, 1).getDay();
+  
+    // Create a table
+    const table = document.createElement("table");
+    calendar.appendChild(table);
+  
+    // Create the header row
+    const headerRow = document.createElement("tr");
+    table.appendChild(headerRow);
+  
+    const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  
+    for (let i = 0; i < 7; i++) {
+      const th = document.createElement("th");
+      th.textContent = daysOfWeek[i];
+      headerRow.appendChild(th);
+    }
+  
+    // Create the calendar cells
+    let date = 1;
+  
+    for (let i = 0; i < 6; i++) {
+      const row = document.createElement("tr");
+  
+      for (let j = 0; j < 7; j++) {
+        if (i === 0 && j < firstDay) {
+          const td = document.createElement("td");
+          row.appendChild(td);
+        } else if (date > numDays) {
+          break;
+        } else {
+          const td = document.createElement("td");
+          td.textContent = date;
+          row.appendChild(td);
+          date++;
+        }
+      }
+  
+      table.appendChild(row);
+    }
+  }
+  
+  // Usage example: create a calendar for June 2023
+  createCalendar(2023, 5);
+  
